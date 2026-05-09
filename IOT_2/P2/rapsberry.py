e109f7b2ea928ee2aa85e4f480807d5a4a4b3e4d
Julia Ayumi Watanabe Iwazawa <julia_42817@aluno.eseg.edu.br>
2026-05-08 22:55:20 -0300

    Add files via upload

---
+++ IOT_2/P2/rapsberry.py
@@ -0,0 +1,18 @@
+import machine
+import machine
+import utime
+potentiometer = machine.ADC(26)
+led_verde = machine.Pin(15, machine.Pin.OUT)
+led_vermelho = machine.Pin(14, machine.Pin.OUT)
+conversion_factor = 1023  / (65535)
+while True:
+    voltage = potentiometer.read_u16() * conversion_factor
+    print(voltage)
+    utime.sleep(2)
+    if voltage > 635:
+        led_verde.value(0)
+        led_vermelho.value(1)
+    else:
+        led_vermelho.value(0)
+        led_verde.value(1)
+        
